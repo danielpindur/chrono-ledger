91927f56a49d7c0d41e8ed9f71bfa3310ce58ebb
Daniel Pindur <pindurdan@gmail.com>
2025-03-12 13:57:32 +0100

    fix versions and nullability

--- src/ChronoLedger.Schema/Users/UserDto.cs
+++ src/ChronoLedger.Schema/Users/UserDto.cs
@@ -4,7 +4,7 @@ public class UserDto
 {
     public Guid UserId { get; set; }
     
-    public string ExternalUserId { get; set; }
+    public required string ExternalUserId { get; set; }
     
     public DateTime CreatedDateTime { get; set; }
 }
